e7d9dfc7c55bf8c68f750880b8101271a02576b7
Mark <goochmd@students.wmlcps.org>
2025-09-24 11:16:42 -0400

    adds buzzer cuz fun

--- Basic Automation/ultrasonic.py
+++ Basic Automation/ultrasonic.py
@@ -1,10 +1,11 @@
-from pitop import UltrasonicSensor, Pitop, ServoMotor
+from pitop import UltrasonicSensor, Pitop, ServoMotor, Buzzer
 import asyncio as aio
 from pitop.robotics import DriveController
 
 ultrasonic = UltrasonicSensor("D3", threshold_distance=1)
 drive = DriveController("M2", "M3")
 panservo = ServoMotor("S0")
+buzzer = Buzzer("D2")
 panservo.target_angle = 0
 miniscreen = Pitop().miniscreen
 global msgchin, msgchout
@@ -17,6 +18,7 @@ async def main():
             msgchout = False
             info = "Obstacle Detected! Stopping."
             if not msgchin:
+                buzzer.blink(0.1, 0.1, 5)
                 print(info)
                 miniscreen.display_multiline_text(info, font_size=14)
                 msgchin = True
